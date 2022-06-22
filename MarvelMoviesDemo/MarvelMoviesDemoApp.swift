c01b8c6a1c293c33b1476e09bf2a7eb331f631ac
Liviu-Paul Bosbiciu <bosbiciu.liviu@gmail.com>
2022-06-22 12:26:49 +0300

    Initial Commit

---
+++ MarvelMoviesDemo/MarvelMoviesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelMoviesDemoApp.swift
+//  MarvelMoviesDemo
+//
+//  Created by Liviu Bosbiciu on 22.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MarvelMoviesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
