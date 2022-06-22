582da3150f87386fc1ab95f63e7499b4c0fe6f16
Liviu-Paul Bosbiciu <bosbiciu.liviu@gmail.com>
2022-06-23 02:07:02 +0300

    Define the endpoints

---
+++ MarvelMoviesDemo/Network/Endpoint.swift
@@ -0,0 +1,28 @@
+//
+//  Endpoint.swift
+//  MarvelMoviesDemo
+//
+//  Created by Liviu Bosbiciu on 22.06.2022.
+//
+
+import Foundation
+
+public enum Endpoint: Equatable {
+    case login
+    case movies
+    case movieDetails(movieId: String)
+    case titleImage(title: String)
+    
+    func path() -> String {
+        switch self {
+        case .login:
+            return "/api/login"
+        case .movies:
+            return "/api/movies"
+        case let .movieDetails(movieId):
+            return "/api/movies/\(movieId)"
+        case let .titleImage(title):
+            return "/images/\(title)"
+        }
+    }
+}
